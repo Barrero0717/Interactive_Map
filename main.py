5d9870a08970d0a8d5c5c706ea780a7c35ae16d6
Andres Felipe Barrero Arce <felipe.barrero17@gmail.com>
2021-09-17 10:24:20 -0500

    Adding new functions

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import folium
+import pandas
 
 def map():
-    map = folium.Map(location=[4.757379055732696, -74.10607339701325], zoom_start=100)
+    # Read the data
+    data = pandas.read_csv("bomberos_bogota.csv")
+    # Extract the necessary data
+    lat = list(data["latitude"])
+    lon = list(data["longitude"])
+    name = list(data["name"])
+
+    # Adding HTML on Popups
+    html = """<h4>Nombre:</h4>
+    %s
+    """
+
+    # Create a Map with folium indicated the coordinates and the zoom_start property
+    map = folium.Map(location=[4.60971, -74.08175], zoom_start=12)
+    # create a group with the same map
     fg = folium.FeatureGroup(name="My Map")
-    map.add_child(folium.Marker(location=[4.757379055732696, -74.10607339701325], popup="Soy un Marcador", icon=folium.Icon(color="blue")))
+    
+    # Add a markers into the map, with a popup and a color
+    for lt, ln, nam in zip(lat, lon, name):
+        # Add the html
+        iframe = folium.IFrame(html=html % str(nam), width=200, height=100)
+        #Put the markers
+        fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color="blue")))
+    
     map.add_child(fg)
+    # Save the map in a file
     map.save("Map1.html")
 
 if __name__ == '__main__':
