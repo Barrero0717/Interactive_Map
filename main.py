2f797485945b762e48545fa7df41ba95d70dad77
Andres Felipe Barrero Arce <felipe.barrero17@gmail.com>
2021-09-20 19:08:43 -0500

    Adding Control Layers

--- main.py
+++ main.py
@@ -2,37 +2,68 @@ import folium
 import pandas
 
 def map():
-    # Read the data
+    # Read and extract the firefighters data
     data = pandas.read_csv("bomberos_bogota.csv")
-    # Extract the necessary data
     lat = list(data["latitude"])
     lon = list(data["longitude"])
     name = list(data["name"])
+    address = list(data["address"])
+    phone = list(data["phone"])
 
-    # Adding HTML on Popups
-    html = """<h4>Nombre:</h4>
-    %s
+    # Adding HTML on Popups firefighters
+    html = """    
+    <head><style>
+    body {
+        font-family: Verdana, sans-serif;
+    }
+    </style></head>
+    <body>
+    <h6>Nombre: %s</h6>
+    <h6>Dirección: %s</h6>
+    <h6>Teléfono: %s</h6>
+    </body>     
     """
+
+    html2 = """    
+    <head><style>
+    body {
+        font-family: Verdana, sans-serif;
+    }
+    </style></head>
+    <body>
+    <h6>Localidad: %s</h6>
+    </body>     
+    """
+   
     # Create a Map with folium indicated the coordinates and the zoom_start property
     map = folium.Map(location=[4.60971, -74.08175], zoom_start=12)
-    # create a group with the same map
-    fg = folium.FeatureGroup(name="My Map")
+    
+    # Create a localities group in the same map
+    loc = folium.FeatureGroup(name="Localidades")
 
     # Adding Bogota localities Map Layer
-    fg.add_child(folium.GeoJson(data=open("localidades.json","r",encoding="utf-8-sig").read(),popup=folium.GeoJsonPopup(fields=['LocNombre'])))
+    loc.add_child(folium.GeoJson(data=open("localidades.json","r",encoding="utf-8-sig").read(),popup=folium.GeoJsonPopup(fields=['LocNombre'],labels=False)))
 
+
+    # Create a firefighters group in the same map
+    fire = folium.FeatureGroup(name="Bomberos")
 
     # Add a markers into the map, with a popup and a color
-    for lt, ln, nam in zip(lat, lon, name):
+    for lt, ln, nam, addr, phon in zip(lat, lon, name, address, phone):
         # Add the html
-        iframe = folium.IFrame(html=html % str(nam), width=200, height=100)
+        iframe = folium.IFrame(html=html % (nam, addr,phon), width=200, height=140)
         # Put the icon markers
         # fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color="blue")))
-        # Replace the icon markers with circle markers
-        fg.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=str(nam), fill_color="blue", color="grey", fill_opacity=0.7 )) 
+        # Put the icon markers with circle markers
+        fire.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=folium.Popup(iframe), fill_color="blue", color="grey", fill_opacity=0.7 )) 
         
+    # Add the groups into the map
+    map.add_child(loc)
+    map.add_child(fire)
+
+    # Add que Layer Control
+    map.add_child(folium.LayerControl())
     
-    map.add_child(fg)
     # Save the map in a file
     map.save("Map1.html")
 
